ed41ba1a2826fc888b4779f79cb158f78f82dab5
Bernhard Gebetsberger <bgebetsb@student.42vienna.com>
2025-10-01 00:45:19 +0200

    fix: misc build/packaging fixes
    fix: Typo in Makefile (Docker was written upper case)
    fix: Change env variables in compose files to HTTPS, add missing API_GATEWAY_UPSTREAM variable to game-service
    fix: Remove non-existent call to 'make cert', fix order of arguments for the "docker:up" script
    fix: Add missing packages to shell.nix

--- shell.nix
+++ shell.nix
@@ -3,11 +3,14 @@
 pkgs.mkShell {
   nativeBuildInputs = with pkgs.buildPackages; [
     bash-language-server
+    cargo
     docker-compose-language-service
     docker-language-server
     gnumake
     nodejs
     openssl
+    pkg-config
+    rustc
     rust-analyzer
     typescript-language-server
     vscode-langservers-extracted
